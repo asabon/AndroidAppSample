e22f7d50c159799b999ad6aa830ebac4f3a9c493
Takafumi Asahara <asabon.t@gmail.com>
2025-01-24 17:54:51 +0900

    coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.kover) apply false
     id("org.jlleitschuh.gradle.ktlint") version "12.1.2" apply false
 }
