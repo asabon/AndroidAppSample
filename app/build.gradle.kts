f040e6b67c3820a5d7dde9134a864663ca749516
Takafumi Asahara <asabon.t@gmail.com>
2025-05-16 17:57:03 +0900

    Change targetSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "net.asabon.androidappsample"
         minSdk = 28
         //noinspection EditedTargetSdkVersion
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
