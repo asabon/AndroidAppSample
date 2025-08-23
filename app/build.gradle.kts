035837795a2721c0ada2da50ce69e838a0fd3053
Takafumi Asahara <asabon.t@gmail.com>
2025-08-23 22:07:17 +0900

    全体的に更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,21 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.kover)
-    alias(libs.plugins.kotlin.ktlint)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.kover)
 }
 
 android {
     namespace = "net.asabon.androidappsample"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "net.asabon.androidappsample"
         minSdk = 28
-        //noinspection EditedTargetSdkVersion
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -23,22 +25,35 @@ android {
 
     buildTypes {
         release {
+            isDebuggable = false
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
+                "proguard-rules.pro"
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+
     buildFeatures {
         compose = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+
+    lint {
+        xmlReport = true
+        htmlReport = true
+        checkReleaseBuilds = false
+        abortOnError = false
     }
 }
 
@@ -68,7 +83,7 @@ ktlint {
     debug = true
     ignoreFailures = true
     reporters {
-        reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+        reporter(ReporterType.CHECKSTYLE)
     }
     filter {
         include("app/src/main/java/**")
